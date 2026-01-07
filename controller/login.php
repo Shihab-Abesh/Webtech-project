a34ee3214f413fb21689ff008d21b45e32eb9def
tawsifhasnat70-learn <tawsifhasnat70@gmail.com>
2026-01-07 23:10:35 +0600

    Add session handling and cookie management for login

--- controller/login.php
+++ controller/login.php
@@ -3,6 +3,24 @@ session_start();
 require_once('../model/userModel.php');
 require_once('../model/adminModel.php');
 
+if (!isset($_SESSION['admin_id']) && isset($_COOKIE['login_email']) && isset($_COOKIE['login_role'])) {
+   if ($_COOKIE['login_role'] === 'admin') {
+       $_SESSION['admin_id'] = $_COOKIE['login_email'];
+       header('location:../view/admin_dashboard.php');
+       exit();
+   }
+   if ($_COOKIE['login_role'] === 'customer') {
+       $_SESSION['customer_email'] = $_COOKIE['login_email'];
+       header('location:../view/cdashboard.php');
+       exit();
+   }
+   if ($_COOKIE['login_role'] === 'ship owner') {
+       $_SESSION['owner_email'] = $_COOKIE['login_email'];
+       header('location:../view/owner_home.php');
+       exit();
+   }
+}
+
 $msg = "";
 
 if(isset($_POST['login'])){
@@ -11,7 +29,6 @@ if(isset($_POST['login'])){
     $password = $_POST['password'];
 
     if($email == "" || $password == ""){
-         header('location: login.php');
         $msg = "null submission!";
     }else{
 
@@ -20,6 +37,8 @@ if(isset($_POST['login'])){
         if(adminLogin($admin)){
             $_SESSION['admin_id'] = $email;
             header('location:../view/admin_dashboard.php');
+            setcookie('login_email', $email, time() + (30), "/");
+            setcookie('login_role', 'admin', time() + (30), "/");
             exit();
         }
 
@@ -33,24 +52,33 @@ if(isset($_POST['login'])){
                 $_SESSION['customer_name'] = $row['name'];
                 $_SESSION['customer_email'] = $row['email'];
                 $_SESSION['customer_role'] = $row['role'];
+                setcookie('login_email', $row['email'], time() + (30), "/");
+                setcookie('login_role', 'customer', time() + (30), "/");
 
                 header('location:../view/cdashboard.php');
                 exit();
             }
-
+           if($row['role'] == 'ship owner'){
             $_SESSION['owner_id'] = $row['id'];
             $_SESSION['owner_name'] = $row['name'];
             $_SESSION['owner_email'] = $row['email'];
             $_SESSION['owner_role'] = $row['role'];
+            setcookie('login_email', $row['email'], time() + (30), "/");
+            setcookie('login_role', 'ship owner', time() + (30), "/");
+
 
             header('location: ../view/owner_home.php');
             exit();
 
+        }
         }else{
             $msg = "invalid user!";
-        }
+            
     }
 }
+}
+
+ 
 ?>
 
 <!DOCTYPE html>
